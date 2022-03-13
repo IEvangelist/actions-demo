8096c66237b8555b51c15352ffa4280da266dc88
David Pine <david.pine@microsoft.com>
2022-03-13 22:43:21 +0000

    Use latest #blazorators bits

--- Blazor.ClientApp/Program.cs
+++ Blazor.ClientApp/Program.cs
@@ -6,7 +6,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddInProcessJavaScript();
+builder.Services.AddLocalStorageServices();
 builder.Services.AddScoped(
     serviceProvider => new HttpClient
     {
